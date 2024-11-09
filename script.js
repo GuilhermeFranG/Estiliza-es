0c2d3965d84071a0db29817c7a2daa886f7c77a2
GuilhermeFranG <qhilherme@gmail.com>
2024-11-09 14:01:17 -0300

    Ajustes Finais.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var cssLink = document.querySelector('link[rel="stylesheet"]');
-var estilos = ["styles/simples.css", "styles/moderno.css", "styles/profissional.css", ""];
+var estilos = ["styles/simples.css", "styles/moderno.css", "styles/profissional.css", "styles/colorido.css", ""];
 
 const simpleStyle = document.querySelectorAll('.simple');
 const modenrStyle = document.querySelectorAll('.modern');
 const profesionalStyle = document.querySelectorAll('.pro');
+const colorStyle = document.querySelectorAll('.color');
 const noStyle = document.querySelectorAll('.empty');
+
 
 simpleStyle.forEach(function (simple) {
     simple.addEventListener('click', function () {
@@ -24,8 +26,14 @@ profesionalStyle.forEach(function (pro) {
     })
 })
 
+colorStyle.forEach(function (color) {
+    color.addEventListener('click', function () {
+        cssLink.href = estilos[3];
+    })
+})
+
 noStyle.forEach(function (empty) {
     empty.addEventListener('click', function () {
-        cssLink.href = estilos[3];
+        cssLink.href = estilos[4];
     })
 })
